4b40caba4c9742256bfe9da2ee57ff27c9a81aea
Michel <106674127+MichelZero@users.noreply.github.com>
2022-06-23 10:40:34 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+#
+#
+from colaborador import Colaborador # Importa a classe Colaborador
+
+#
+# Cria um objeto da classe Colaborador
+c1 = Colaborador('João', 'vendedor', '1234567890', 1990)
+#
+# Imprime
+print(c1.hoje)
